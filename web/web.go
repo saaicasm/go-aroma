b0a07dbee7be9e076e3c1251384ce3d4b0cd2666
iamlego <sairaj.jadhav812@gmail.com>
2024-11-28 18:07:23 -0800

    web func

--- web/web.go
+++ web/web.go
@@ -6,18 +6,35 @@ import (
 	"net/http"
 )
 
+const port = ":8080"
+
+// Function handler for Home
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is Home Page")
+}
+
+// Function handler for About
+func About(w http.ResponseWriter, r *http.Request) {
+	var sum int
+	sum = addValues(6, 9)
+
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the About Page and 6 + 9 is %d ", sum))
+
+}
+
+// Adds two intergers
+func addValues(x int, y int) int {
+	return x + y
+}
+
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//Routes for Home and About
 
-		_, err := fmt.Fprintf(w, "Hello World")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/About", About)
 
-		if err != nil {
-			log.Println("Error", err)
-		}
-
-	})
-
-	http.ListenAndServe(":8080", nil)
-
+	//Starts the Server on the port provided
+	log.Println(fmt.Sprintf("The server is running on Port %s", port))
+	_ = http.ListenAndServe(port, nil)
 }
