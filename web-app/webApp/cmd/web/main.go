fc0b9514160ac5845a7463c3c39ba1587b10ef3c
iamlego <sairaj.jadhav812@gmail.com>
2024-11-29 16:02:48 -0800

    dev n prod

--- web-app/webApp/cmd/web/main.go
+++ web-app/webApp/cmd/web/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github/iamlego/go-web/pkg/config"
 	handler "github/iamlego/go-web/pkg/handlers"
+	"github/iamlego/go-web/pkg/render"
 	"log"
 	"net/http"
 )
@@ -10,8 +12,24 @@ import (
 const port = ":3000"
 
 func main() {
-	http.HandleFunc("/", handler.Home)
-	http.HandleFunc("/About", handler.About)
+
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cant create templ cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handler.NewRepo(&app) // get repo from handler and add app to it
+	handler.NewHandler(repo)      // send back the updated repo with tc to handler
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handler.Repo.Home)
+	http.HandleFunc("/About", handler.Repo.About)
 
 	log.Println(fmt.Sprintf("The server is running on Port %s", port))
 	_ = http.ListenAndServe(port, nil)
--- web-app/webApp/pkg/render/render.go
+++ web-app/webApp/pkg/render/render.go
@@ -2,75 +2,43 @@ package render
 
 import (
 	"bytes"
+	"github/iamlego/go-web/pkg/config"
 	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 )
 
-// var tc = make(map[string]*template.Template)
 
-// func RenderTemplate(w http.ResponseWriter, t string) {
-// 	var tmpl *template.Template
-// 	var err error
+var app *config.AppConfig
 
-// 	_, inMap := tc[t]
-// 	if !inMap {
-// 		// need to create the template
-// 		log.Println("creating template and adding to cache")
-// 		err = createTemplateCache(t)
-// 		if err != nil {
-// 			log.Println(err)
-// 		}
-// 	} else {
-
-// 		log.Println("using cached template")
-// 	}
-
-// 	tmpl = tc[t]
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		log.Println(err)
-// 	}
-// }
-
-// func createTemplateCache(t string) error {
-// 	templates := []string{
-// 		fmt.Sprintf("./templates/%s", t),
-// 		"./templates/base.layout.tmpl",
-// 	}
-
-// 	// parse the template
-// 	tmpl, err := template.ParseFiles(templates...)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	tc[t] = tmpl
-
-// 	return nil
-// }
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	var tc map[string]*template.Template
+	if app.UseCache {
+		// get template cache from app config
+		tc = app.TemplateCache
 
-	log.Println("This is the tmpl : ", tmpl)
-	//create a template
-	tc, err := createTemplateCache()
-	if err != nil {
-		log.Fatal(err)
+	}else{
+		tc, _ = CreateTemplateCache()
 	}
+
+
+	
 
 	//get the template from cache
 	t, ok := tc[tmpl]
 
 	if !ok {
-		log.Fatal(err)
+		log.Fatal("Error getting template")
 	}
 
 	buf := new(bytes.Buffer)
 
-	err = t.Execute(buf, nil)
+	err := t.Execute(buf, nil)
 	if err != nil {
 		log.Println(err)
 	}
@@ -83,29 +51,22 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 
 }
 
-func createTemplateCache() (map[string]*template.Template, error) {
+func CreateTemplateCache() (map[string]*template.Template, error) {
 	//will take no args
 	//will return map of string -> template , error
 
-	//1 - create an empty map of string -> template
+
 	myCache := map[string]*template.Template{}
 
-	log.Println("This should be empty cache ", myCache)
-
-	//2 - get all the pages with file named *.page.tmpl from ./templates
 	pages, err := filepath.Glob("../.././templates/*.page.tmpl")
 
-	log.Println("This should be all names of page.tmpls ", pages, err)
 
 	if err != nil {
 		return myCache, err
 	}
 
-	//3 - range through all the files ending with *.page.tmpl
 	for _, page := range pages {
 		name := filepath.Base(page)
-
-		log.Println("The name of the page ", page, "is ", name)
 
 		ts, err := template.New(name).ParseFiles(page)
 		if err != nil {
@@ -116,8 +77,6 @@ func createTemplateCache() (map[string]*template.Template, error) {
 		if err != nil {
 			return myCache, err
 		}
-
-		log.Println("The strings that matches layouts ", matches)
 
 		if len(matches) > 0 {
 			ts, err = ts.ParseGlob("../.././templates/*layout.tmpl")
@@ -130,7 +89,6 @@ func createTemplateCache() (map[string]*template.Template, error) {
 
 	}
 
-	log.Println("My cache at the end", myCache)
 
 	return myCache, nil
 
