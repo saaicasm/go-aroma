f36e8faa9d7f4148ba176fa058d919ad33c4e2db
iamlego <sairaj.jadhav812@gmail.com>
2025-03-12 02:01:20 -0700

    psql

--- odyssey/types.go
+++ odyssey/types.go
@@ -1,20 +1,29 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
+
+type CreateAccountRequest struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
 
 type Account struct {
-	ID        int `json:"id"`
-	FirstName string `json:"firstName"`
-	Lastname  string `json:"lastName"`
-	Number    int64 `json:"number"`
-	Balance   int64 `json:"balance"`
+	ID        int       `json:"id"`
+	FirstName string    `json:"firstName"`
+	Lastname  string    `json:"lastName"`
+	Number    int64     `json:"number"`
+	Balance   int64     `json:"balance"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 func NewAccount(firstname, lastname string) *Account {
 	return &Account{
-		ID:        rand.Intn(1000),
 		FirstName: firstname,
 		Lastname:  lastname,
 		Number:    int64(rand.Intn(10000000)),
+		CreatedAt: time.Now().UTC(),
 	}
 }
